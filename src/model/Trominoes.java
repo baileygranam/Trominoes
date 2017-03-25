d57e3d3075f54bca0035b7c2f8472753ab32163a
Bailey Granam <bgranam@stetson.edu>
2017-03-25 12:53:40 -0400

    Added a few methods for setting variables

---
+++ src/model/Trominoes.java
@@ -0,0 +1,46 @@
+package model;
+
+/**
+ * This program tiles with right trominoes using an (n x n) board
+ * with one square missing, assuming that n is a power of 2.
+ * 
+ * @author Bailey Granam 
+ * @author Mimi Shimkovska 
+ *
+ */
+
+public class Trominoes 
+{
+	
+	private int myX; // X Coordinate of board
+	private int myY; // Y Coordinate of board
+	private int myXMissing; // X Coordinate of missing square
+	private int myYMissing; // Y Coordinate of missing square
+	private int myBoardSize; // Size of board
+	
+	public Trominoes()
+	{
+		myX = 0;
+		myY = 0;
+		
+	}
+	
+	public void setBoardSize(int boardSize) 
+	{
+		myBoardSize = (int) java.lang.Math.pow(2,boardSize);
+	}
+	
+	public void setMissingSquare(int x, int y)
+	{
+		myXMissing = x;
+		myYMissing = y;
+	}
+	
+	
+	public void tileBoard()
+	{
+		
+	}
+	
+	
+}
